0e56b123cb2e59bb23cf157a2bda5aca8377517e
Killerzelle <neuberger.julian@gmail.com>
2018-05-14 10:54:07 +0200

    added players to admin menu

--- turns/admin.py
+++ turns/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from turns.models import Dialogue, Sentence, Intent, Parameter, IntentTemplate, ParameterTemplate
+from turns.models import Dialogue, Sentence, Intent, Parameter, IntentTemplate, ParameterTemplate, Player
 
 
-@admin.register(Dialogue, Sentence, Intent, Parameter, IntentTemplate, ParameterTemplate)
+@admin.register(Dialogue, Sentence, Intent, Parameter, IntentTemplate, ParameterTemplate, Player)
 class TurnAdmin(admin.ModelAdmin):
     pass
